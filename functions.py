07e00f94258c917cf755c5bce9d8d7fb1633e944
Krzysztof Krzeszowski <krzysztof.krzeszowski@gmail.com>
2017-04-12 12:23:52 +0200

    docstrings

--- functions.py
+++ functions.py
@@ -1,5 +1,16 @@
 import h5py
 
+def get_mean_profile(d):
+    """ Get mean profile from all pulses
+
+    d - ndarray with single pulse data
+    """
+    return d.mean(axis=0)
+
 def read_data(f):
-    return h5py.File(f)['data'][:]
+    """ Get ndarray with single pulses
+
+    f - path to hdf5 file
+    """
+    return h5py.File(f.as_posix())['data'][:]
     
--- single-analysis.py
+++ single-analysis.py
@@ -3,12 +3,25 @@
 import functions as fn
 import sys
 
+from pathlib import Path
+
+CONFIG = {
+    'WINDOW_SIZE': 500,
+    'WINDOW_STEP': 100,
+}
+
 if len(sys.argv) != 2:
     print('Usage:')
     print('single-analysis.py hdf5_data_file')
     exit()
 
-f = sys.argv[1]
+f = Path(sys.argv[1])
+
+if not f.is_file():
+    exit('File does not exist')
 
 d = fn.read_data(f)
-print(d.shape)
+
+for f in fn.get_mean_profile(d):
+    print(f)
+
