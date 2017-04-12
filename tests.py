972c7d0046ce9c611820ce65ff6cbcd545da23b4
Krzysztof Krzeszowski <krzysztof.krzeszowski@gmail.com>
2017-04-12 12:34:08 +0200

    unit test file

---
+++ tests.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import functions as fn
+import numpy as np
+import unittest
+
+
+class Test(unittest.TestCase):
+    def mean_profile():
+        d = np.array([[1, 2, 3], [2, 3, 4], [3, 4, 5]])
+        seld.assertEqual(d, np.array([2, 3, 4]))
+
+if __name__ == '__main__':
+    unittest.main()
