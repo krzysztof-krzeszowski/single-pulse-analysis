e04d6f0c9cba38fe20bc2884a896dfede0e85cc7
Krzysztof Krzeszowski <krzysztof.krzeszowski@gmail.com>
2017-04-12 13:33:45 +0200

    get sd from all pulses

--- functions.py
+++ functions.py
@@ -2,14 +2,21 @@ import h5py
 import numpy as np
 
 def get_sd_from_pulse(d, width):
-    """ Get minimum and maximum value of standard deviation 
+    """ Get minimum and maximum value of standard deviation from single pulse
 
     d - 1-d array
     width - width of sd calculation
-    step - starting point of next window 
     """
     sd = np.std(rolling_window(d, width), axis=1)
     return np.array([np.min(sd), np.max(sd)])
+
+def get_sd_from_pulses(d, width):
+    """ Get minimum and maximum values of standard deviation for set of single pulses
+
+    d - 2-d array
+    width - width of sd calculation
+    """
+    return np.array([get_sd_from_pulse(r, width) for r in d])
 
 def get_mean_profile(d):
     """ Get mean profile from all pulses
--- tests.py
+++ tests.py
@@ -10,9 +10,23 @@ class Test(unittest.TestCase):
         d = np.array([[1, 2, 3, 4, 5], [3, 4, 5, 6, 7], [5, 6, 7, 8, 9]])
         np.testing.assert_array_equal(fn.get_mean_profile(d), np.array([3, 4, 5, 6, 7]))
 
-    def test_running_sd(self):
+    def test_running_sd_single(self):
         d = np.array([4, 2, 5, 8, 4, 1, 2, 7, 9, 0, 7, 5, 2, 3, 7, 9, 2])
         np.testing.assert_array_almost_equal(fn.get_sd_from_pulse(d, width=3), np.array([1.2472, 3.8586]), decimal=4)
+
+    def test_running_sd_pulses(self):
+        d = np.array([
+            [9, 4, 7, 7, 4, 4, 4, 6, 0, 5],
+            [6, 4, 4, 7, 8, 3, 6, 6, 8, 9],
+            [9, 6, 2, 1, 0, 3, 9, 9, 2, 8]
+        ])
+        ans = np.array([
+            [1.2649, 2.0396],
+            [1.6000, 2.0591],
+            [2.0591, 3.8781],
+        ])
+
+        np.testing.assert_array_almost_equal(fn.get_sd_from_pulses(d, width=5), ans, decimal=4)
 
 if __name__ == '__main__':
     unittest.main()
